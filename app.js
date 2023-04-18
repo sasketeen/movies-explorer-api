85ed5209afb79d282f8547285123937eb768fe1d
Stas Proydakov <stasprojdakov@icloud.com>
2023-04-18 22:32:24 +0300

    feat: добавил limiter и hemlet

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
 const cors = require('cors');
 
 const { PORT, DBLINK } = require('./appConfig');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { handleErrors } = require('./middlewares/handleErrors');
+const { limiter } = require('./middlewares/limiter');
 const router = require('./routes/index');
 
 const app = express();
@@ -14,7 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use(helmet());
+app.use(requestLogger);
+app.use(limiter);
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 app.use(handleErrors);
 app.listen(PORT, () => {
